f99b6730bc7e2102cc1fd540a568147eb8387125
rhysgilliam <97763270+rhysgilliam@users.noreply.github.com>
2024-09-08 19:21:01 -0400

    Fix routing and update start page bio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { StartPageComponent } from "./start-page/start-page.component";
+import { AboutComponent } from './about/about.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, StartPageComponent]
+    styleUrls: ['./app.component.css'],
+    imports: [RouterOutlet, StartPageComponent, AboutComponent, RouterModule]
 })
 export class AppComponent {
+
+  openResume() {
+    window.open('assets/resume.pdf', '_blank');
+  }
+
   title = 'portfolio';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: StartPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: '**', redirectTo: '/home' }
+];
