81502f6a4c60ced76fbffca91cbe08a441f5bb5c
rhysgilliam <97763270+rhysgilliam@users.noreply.github.com>
2024-11-03 00:02:59 -0400

    Update resume and add new button

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.css'
 })
